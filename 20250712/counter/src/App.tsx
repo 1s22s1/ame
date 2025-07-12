8553fb8b3b7ded7586c8d988e9a037f41189c48c
1s22s1 <1s22s1@users.noreply.github.com>
2025-07-12 10:59:06 +0900

    Add counter

--- 20250712/counter/src/App.tsx
+++ 20250712/counter/src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import './App.css'
+import { Button, Stack } from '@mui/material'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const plus = () => {
+    setCounter(counter + 1)
+  }
+
+  const minus = () => {
+    setCounter(counter - 1)
+  }
 
   return (
     <>
-      <p>おはろー</p>
+      <Stack direction={'row'} spacing={2}>
+        <Button variant="contained" onClick={plus}>+1</Button>
+        <Button variant="contained" onClick={minus}>-1</Button>
+      </Stack>
+
+      <p>{`今は${counter}です。`}</p>
     </>
   )
 }
