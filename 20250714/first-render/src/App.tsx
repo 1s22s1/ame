205f95c404202b8da789ea8546d8f97d5d9b49a8
1s22s1 <1s22s1@users.noreply.github.com>
2025-07-14 09:54:10 +0900

    :up:

---
+++ 20250714/first-render/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import { Button, Stack } from '@mui/material';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [isFirst, setIsFirst] = useState(true);
+
+  const plus = () => {
+    setCount(count + 1);
+    setIsFirst(false);
+  }
+
+  const minus = () => {
+    setCount(count - 1);
+    setIsFirst(false);
+  }
+
+  return (
+    <>
+      <Stack direction={'row'} spacing={2}>
+        <Button variant='outlined' onClick={plus}>+</Button>
+        <Button variant='outlined' onClick={minus}>-</Button>
+      </Stack >
+
+      <p>count is {count}.</p>
+      {isFirst && <p>Now, this is the first.</p>}
+    </>
+  )
+}
+
+export default App
